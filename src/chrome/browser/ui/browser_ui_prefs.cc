d7eb13af9b709e6b58c3615d9900aa488119bf8d
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-11-28 12:34:34 +0100

    Update browser_ui_prefs.cc

--- src/chrome/browser/ui/browser_ui_prefs.cc
+++ src/chrome/browser/ui/browser_ui_prefs.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Copyright (c) 2022 The Chromium MR Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,7 +20,96 @@
 #include "components/translate/core/browser/translate_pref_names.h"
 #include "media/media_buildflags.h"
 #include "third_party/blink/public/common/peerconnection/webrtc_ip_handling_policy.h"
+#include "third_party/blink/public/common/switches.h"
+#include "ui/base/ui_base_features.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
+#include "ui/native_theme/native_theme.h"
+#include "ui/native_theme/native_theme_features.h"
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/public/cpp/ash_pref_names.h"
+#include "ash/public/cpp/ash_switches.h"
+#include "ash/public/cpp/ash_switches.h"
+#include "chromeos/constants/chromeos_features.h"
+#include "chromeos/constants/chromeos_switches.h"
+#include "chromeos/login/login_state/login_state.h"
+#include "chromeos/settings/cros_settings_names.h"
+#include "components/user_manager/user_manager.h"
+#include "ui/aura/window.h"
+#include "ui/aura/window_tree_host.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/constants/chromeos_features.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "extensions/common/extension_features.h"
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+
+#if defined(OS_MAC)
+#include "chrome/browser/ui/cocoa/confirm_quit.h"
+#endif  // defined(OS_MAC)
+
+#if defined(OS_WIN)
+#include "chrome/browser/shell_integration.h"
+#include "chrome/browser/ui/browser_dialogs.h"
+#include "ui/base/ui_base_features.h"
+#endif  // defined(OS_WIN)
+
+#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+#include "chrome/browser/ui/web_applications/app_browser_controller.h"
+#include "chrome/browser/web_applications/components/app_registrar.h"
+#include "chrome/browser/web_applications/components/web_app_provider_base.h"
+#include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/common/chrome_features.h"
+#include "components/keep_alive_registry/keep_alive_types.h"
+#include "components/keep_alive_registry/scoped_keep_alive.h"
+#endif  // defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+
+#if defined(OS_ANDROID)
+#include "chrome/browser/android/preferences/browser_prefs_android.h"
+#include "chrome/browser/android/preferences/pref_service_bridge.h"
+#include "chrome/browser/ui/android/tab_model/tab_model_list.h"
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_list_observer.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/webui/settings/chrome_cleanup_handler
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/public/cpp/ash_pref_names.h"
+#include "ash/public/cpp/tablet_mode.h"
+#include "ash/public/cpp/tablet_mode_observer.h"
+#include "chrome/browser/ash/settings/cros_settings.h"
+#include "chrome/browser/ash/settings/device_settings_service.h"
+#include "chrome/browser/ash/settings/scoped_cros_settings_test_helper.h"
+#include "chrome/browser/ash/system/timezone_resolver_manager.h"
+#include "chromeos/constants/chromeos_features.h"
+#include "chromeos/settings/cros_settings_names.h"
+#include "components/user_manager/user_manager.h"
+#include "components/user_manager/user_type.h"
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/ash/crosapi/browser_util.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_chrome_impl.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_chrome_impl_factory.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_factory.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_impl.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_impl_factory.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_impl_factory_impl.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_impl_factory_impl_factory.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_impl_factory_impl_factory_impl.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_impl_factory_impl_factory_impl_singleton.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_impl_factory_impl_singleton.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_singleton.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_singleton_factory.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_singleton_factory_impl.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_singleton_factory_impl_singleton.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_singleton_singleton.h"
+#include "chrome/browser/ash/crosapi/browser_util_lacros_singleton_singleton_factory.h"
+#include "chrome/browser/ash/crosapi
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ui/accessibility/accessibility_features.h"
 #endif
@@ -117,6 +206,7 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(prefs::kClickToCallEnabled, true);
 #endif  // BUILDFLAG(ENABLE_CLICK_TO_CALL)
 
+
 #if defined(OS_MAC)
   // This really belongs in platform code, but there's no good place to
   // initialize it between the time when the AppController is created
@@ -166,3 +256,4 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
       prefs::kHttpsOnlyModeEnabled, false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 }
+
